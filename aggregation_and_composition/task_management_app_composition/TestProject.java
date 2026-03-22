ad9dd3d36cdb1197fe8a097b1ce200e307f80307
isaque.lucio <isaque.o.lucio@gmail.com>
2026-03-21 21:15:45 -0300

    Exercises of agregation and composition

---
+++ aggregation_and_composition/task_management_app_composition/TestProject.java
@@ -0,0 +1,20 @@
+package aggregation_and_composition.task_management_app_composition;
+
+public class TestProject {
+    public static void main(String[] args) {
+        
+        Project pro1 = new Project("Online Museum");
+        pro1.createSprint(1, "First Screen");
+        pro1.createSprint(2, "Second Screen");
+        pro1.createSprint(3, "Third Screen");
+        System.out.println(pro1);
+
+        pro1 = null;
+        
+        if(pro1 == null){
+            System.out.println("proving that composition is working, cause now the list is null: ");
+            System.out.println(pro1.getSprintList());
+        } //Without "else" because it will never fall here, since on line 18 the list was set as null.
+        
+    }
+}
